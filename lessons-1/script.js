24a9bcdf0a248a8b27035ebd6010da6c4f95f246
Ekaterina Karas <eartsman@mail.ru>
2019-10-17 21:14:02 +0200

    Initial commit

---
+++ lessons-1/script.js
@@ -0,0 +1,12 @@
+var tc = + prompt ( 'Введите температуру в градусах по Цельсию: ' ),
+    tf = 1.8*tc + 32;
+alert ( 'Температура по Фаренгейту: ' + tf + ' ℉' );
+
+var admin,
+    name;
+name = 'Василий';
+admin = name;
+alert ( admin );
+
+var x = 1000 + "108";
+alert ( x );
